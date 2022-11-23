c5eb29b5cbf10531577bd2fb80a64a2928b34ad6
Docsilva <douglas.csilva@mercadolivre>
2022-11-23 12:06:21 -0300

    Atualização projeto hallo
    Atualização dos imports

--- hallo/src/main/java/com/generation/hallo/controller/HelloController.java
+++ hallo/src/main/java/com/generation/hallo/controller/HelloController.java
@@ -1,8 +1,8 @@
 package com.generation.hallo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
