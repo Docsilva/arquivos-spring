7e372f90e5ced991c71c78e58fbafe73f1c747d6
Docsilva <douglas.csilva@mercadolivre.com>
2022-12-16 18:04:37 -0300

    projeto farmacia
    Correção da classe repository

--- projFarmacia/src/main/java/com/generation/projFarmacia/repository/ProdutoRepository.java
+++ projFarmacia/src/main/java/com/generation/projFarmacia/repository/ProdutoRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.projFarmacia.model.Produto;
 
 
-
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	
 	public List <Produto> findByNomeContainingIgnoreCase(@Param("nome") String nome);
